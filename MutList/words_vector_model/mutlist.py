2214e83387cae05a61f2ad91a4404b62eb60a31d
Pedro Matos <elitdruid@gmail.com>
2018-03-05 18:46:38 +0000

    created the graph. It's still missing the functions to get the batchs

--- MutList/words_vector_model/mutlist.py
+++ MutList/words_vector_model/mutlist.py
@@ -1,6 +1,8 @@
 from preprocess import PreProcess
 from sklearn.model_selection import train_test_split
 import numpy as np
+import tensorflow as tf
+import datetime
 
 class Mutlist:
     def __init__(self):
@@ -44,6 +46,71 @@ class Mutlist:
 
         return train_df, test_df, labels_train, labels_test
 
+    def train_model(self):
+        tf.reset_default_graph()
+
+        labels = tf.placeholder(tf.float32, [self.batchSize, self.numClasses])
+        input_data = tf.placeholder(tf.int32, [self.batchSize, self.maxSeqLength])
+
+        data = tf.Variable(tf.zeros([self.batchSize, self.maxSeqLength, self.numDimensions]), dtype=tf.float32)
+        data = tf.nn.embedding_lookup(self.wordVectors, input_data)
+
+        lstmCell = tf.contrib.rnn.BasicLSTMCell(self.lstmUnits)
+        lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
+        value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+
+        weight = tf.Variable(tf.truncated_normal([self.lstmUnits, self.numClasses]))
+        bias = tf.Variable(tf.constant(0.1, shape=[self.numClasses]))
+        value = tf.transpose(value, [1, 0, 2])
+        last = tf.gather(value, int(value.get_shape()[0]) - 1)
+        prediction = (tf.matmul(last, weight) + bias)
+
+        # The correct prediction formulation works by looking at the index of the maximum value of the output values,
+        #  and then seeing whether it matches with the training labels.
+        correctPred = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
+        optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+        # create the Session to run the graph
+        sess = tf.InteractiveSession()
+        saver = tf.train.Saver()
+        sess.run(tf.global_variables_initializer())
+
+        # to use TensorBoard
+        tf.summary.scalar('Loss', loss)
+        tf.summary.scalar('Accuracy', accuracy)
+        merged = tf.summary.merge_all()
+        logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+        writer = tf.summary.FileWriter(logdir, sess.graph)
+
+        for i in range(self.iterations):
+            # Next Batch of reviews
+            nextBatch, nextBatchLabels = getTrainBatch()
+            sess.run(optimizer, {input_data: nextBatch, labels: nextBatchLabels})
+
+            #Write summary to Tensorboard
+            if (i % 50 == 0):
+                summary = sess.run(merged, {input_data: nextBatch, labels: nextBatchLabels})
+                writer.add_summary(summary, i)
+
+            #Save the network every 10,000 training iterations
+            if (i % 10000 == 0 and i != 0):
+                save_path = saver.save(sess, "models/pretrained_lstm.ckpt", global_step=i)
+                print("saved to %s" % save_path)
+
+        writer.close()
+
+        sess = tf.InteractiveSession()
+        saver = tf.train.Saver()
+        saver.restore(sess, tf.train.latest_checkpoint('models'))
+
+        iterations = 10
+        for i in range(iterations):
+            nextBatch, nextBatchLabels = getTestBatch()
+            print("Accuracy for this batch:",
+                  (sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100)
 
     def main(self):
         preprocess = PreProcess()
