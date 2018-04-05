e6aeb0501baf075cd349132776155e6ec70bff64
Pedro Matos <elitdruid@gmail.com>
2018-04-05 14:12:14 +0100

    the other model is ready to train

--- MutList2/model/charmodel.py
+++ MutList2/model/charmodel.py
@@ -11,18 +11,25 @@ from keras_contrib.layers import CRF
 from collections import defaultdict
 from datetime import datetime
 import sys
+import time
 import random
+import collections
+import matplotlib.pyplot as plt
+
 
 
 class CharModel:
     def __init__(self):
         self.maxSeqLength = -1  # Maximum length of sentence
+        self.w_arit_mean = -1
         self.dic_corpus_char = defaultdict(list)
         self.dic_chars = defaultdict(list)
         self.data = []
         self.alphabet = []
-        self.labdict = {'O': 1, 'B': 2, 'I': 3, 'X': 0}
-        self.lab_len = 4
+        # self.labdict = {'O': 1, 'B': 2, 'I': 3, 'X': 0}
+        # self.lab_len = 4
+        self.dict_labs_nopad = {'O': 0, 'B': 1, 'I': 2}
+        self.num_labs = 3
         self.epochsN = 5
 
     def creat_alphabet(self):
@@ -39,7 +46,7 @@ class CharModel:
         for idx in range(len(self.data)):
             labs = self.data[idx]['labels']
             # convert SOBIE tags to numbers
-            self.data[idx]["bion"] = [self.labdict[i] for i in labs]
+            self.data[idx]["bion"] = [self.dict_labs_nopad[i] for i in labs]
 
     def get_seq(self, DICT):
         sequences = []
@@ -57,7 +64,7 @@ class CharModel:
             # pass labels to its numeric value
             labs_num = []
             for l in labs:
-                labs_num.append(self.labdict.get(l))
+                labs_num.append(self.dict_labs_nopad.get(l))
             labels.append(labs_num)
 
         return sequences, labels
@@ -117,24 +124,20 @@ class CharModel:
             if l1 not in train_l_labels: train_l_labels[l1] = []
             train_l_labels[l1].append(seq['bion'])
 
-
         sizes = list(train_l_d.keys())
 
         # Set up the keras model
-        input = Input(shape=(None,), dtype='int32')
-        el = Embedding(n_char + 1, 200, name="embed")(input)
+        il = Input(shape=(None,), dtype='int32')
+        el = Embedding(n_char + 1, 200, name="embed")(il)
         bl1 = Bidirectional(LSTM(128, return_sequences=True, recurrent_dropout=0.5, dropout=0.5), merge_mode="concat",
                             name="lstm1")(el)
         bl2 = Bidirectional(LSTM(64, return_sequences=True, recurrent_dropout=0.5, dropout=0.5), merge_mode="concat",
                             name="lstm2")(bl1)
         bl3 = Bidirectional(LSTM(64, return_sequences=True, recurrent_dropout=0.5, dropout=0.5), merge_mode="concat",
                             name="lstm3")(bl2)
-        model = TimeDistributed(Dense(50, activation="relu"))(bl3)  # a dense layer as suggested by neuralNer
-        crf = CRF(self.lab_len)  # CRF layer
-        out = crf(model)  # output
-
-        model = Model(input, out)
-        model.compile(optimizer="rmsprop", loss=crf.loss_function, metrics=[crf.accuracy])
+        dl = TimeDistributed(Dense(self.num_labs, activation="softmax"), name="output")(bl3)
+        model = Model(inputs=il, outputs=dl)
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
         model.summary()
 
         # OK, start actually training
@@ -142,25 +145,57 @@ class CharModel:
             print("Epoch", epoch, "start at", datetime.now(), file=sys.stderr)
             # Train in batches of different sizes - randomize the order of sizes
             # Except for the first few epochs - train on the smallest examples first
-            if epoch >3 : random.shuffle(sizes)  # For unknown reasons we can't train on a single token (i.e. character)
+            if epoch > 3:
+                random.shuffle(sizes)  # For unknown reasons we can't train on a single token (i.e. character)
             for size in sizes:
                 if size == 1: continue
                 batch = train_l_d[size]
                 labs = train_l_labels[size]
-                for i in range(len(batch)):
-                    print(len(batch[i]) == len(labs[i]))
-                    print("\n\n")
 
+                tx = np.array([seq for seq in batch])
+                ty = np.array([[to_categorical(i, num_classes=self.num_labs) for i in seq] for seq in labs])
 
-
+                # This trains in mini-batches
+                model.fit(tx, ty, verbose=0, epochs=1)
             print("Trained at", datetime.now(), file=sys.stderr)
 
+    def seqs_distribution(self):
+        dimensions = {}
+        for seq in self.data:
+            l = len(seq['corpus'])
+
+            if l not in dimensions: dimensions[l] = 0
+            value = dimensions.get(l)
+            value = value + 1
+            dimensions[l] = value
+
+        od = collections.OrderedDict(sorted(dimensions.items()))
+
+        df = pd.DataFrame.from_dict(od, orient='index')
+        df.plot(kind='bar', figsize=(50,10))
+        plt.show()
+
+        # WEIGHTED ARITHMETIC MEAN
+        numerator = 0
+        denominator = 0
+        for item in od:
+            value = od.get(item)
+            prod = item * value
+            numerator = numerator + prod
+            denominator = denominator + value
+
+        w_arit_mean = numerator/denominator
+        return w_arit_mean
 
     def main(self):
         reader = seq_to_char.CorpusReader()
         self.data = reader.read()
         self.creat_alphabet()
         self.maxSeqLength = reader.get_length(self.data)
+
+        # sequences length distribution
+        # self.w_arit_mean = self.seqs_distribution()
+
 
         # we associate every character in our alphabet to a number:
         # e.g. b => 1 d => 3 etc.
